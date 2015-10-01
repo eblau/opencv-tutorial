cc72596041fe54792d9c28892488b8c12feb67c8
eblau <eblau@eblau.com>
2015-10-01 19:10:28 -0400

    Revert "Added greeting"

--- DisplayImage.cpp
+++ DisplayImage.cpp
@@ -7,7 +7,6 @@ using namespace std;
 
 int main( int argc, char** argv )
 {
-    cout << "Hi! This is Laura!!\n";
     if( argc != 2)
     {
      cout <<" Usage: display_image ImageToLoadAndDisplay" << endl;
