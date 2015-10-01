09b62bc637383025ff26dae9060e94fa1d099910
Eric Blau <eblau@eblau.com>
2015-10-01 19:06:49 -0400

    Added greeting

--- DisplayImage.cpp
+++ DisplayImage.cpp
@@ -7,6 +7,7 @@ using namespace std;
 
 int main( int argc, char** argv )
 {
+    cout << "Hi! This is Laura!!\n";
     if( argc != 2)
     {
      cout <<" Usage: display_image ImageToLoadAndDisplay" << endl;
